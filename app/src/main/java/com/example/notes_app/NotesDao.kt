4439c1e4b5142414e5635eea54479b7d03793094
Ayush Chauhan <ayushchauhan0707@gmail.com>
2023-03-10 00:27:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notes_app/NotesDao.kt
@@ -0,0 +1,33 @@
+package com.example.notes_app
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+// annotation for dao class.
+@Dao
+interface NotesDao {
+
+    // below is the insert method for
+    // adding a new entry to our database.
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note :Note)
+
+    // below is the delete method
+    // for deleting our note.
+    @Delete
+    suspend fun delete(note: Note)
+
+    // below is the method to read all the notes
+    // from our database we have specified the query for it.
+    // inside the query we are arranging it in ascending
+    // order on below line and we are specifying
+    // the table name from which
+    // we have to get the data.
+    @Query("Select * from notesTable order by id ASC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+    // below method is use to update the note.
+    @Update
+    suspend fun update(note: Note)
+
+}
